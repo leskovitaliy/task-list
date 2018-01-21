199495d08f290fbbd9e9d73cd58d33f0fbb7020f
lesko.vitaliy <lesko.vitaliy90@gmail.com>
2018-01-21 20:49:20 +0200

    add scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import {Task} from './models/task';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   tasks: Array<any>;
--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -5,7 +5,7 @@ import {TasksService} from '../services/tasks.service';
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
-  styleUrls: ['./task-list.component.css']
+  styleUrls: ['./task-list.component.scss']
 })
 export class TaskListComponent {
   @Input() tasks: Array<Task>;
